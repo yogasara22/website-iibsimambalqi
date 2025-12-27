94c13624cfbbed1d77f6adabe91a2c855b9a8575
yogasara <yogasara94@gmail.com>
2025-12-27 16:04:48 +0700

    upgrade version next js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   generateEtags: false,
   poweredByHeader: false,
-  swcMinify: true,
   images: {
-    domains: ['localhost', 'via.placeholder.com', 'placehold.co'],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
